7e5ca4b6b98a1d917c18bf247b9c75ad7c786ee1
Aras Yazgan <aras.yazgan@gmail.com>
2024-01-27 17:11:01 -0800

    Complete base class

--- Mods/Psion/ScriptExtender/Lua/PsionFunctions.lua
+++ Mods/Psion/ScriptExtender/Lua/PsionFunctions.lua
@@ -1,23 +1,23 @@
-Ext.Osiris.RegisterListener("UsingSpell", 5, "before", function (character, spell, _, _, _)
-    if  Osi.HasActiveStatus(character,"CONSUMPTIVE_POWER") then
+Ext.Osiris.RegisterListener("UsingSpell", 5, "before", function (caster, spell, _, _, _)
+    if  Osi.HasActiveStatus(caster,"CONSUMPTIVE_POWER") == 1 then
         local spellData = Ext.Stats.Get(spell)
         local spellLevel = spellData.Level
         _D(spellLevel)
         local lastCharSpell = tonumber(string.sub(spell, -1), 10) -- returns nil if string isn't an integer
          -- For some reason upcasted spells' spellData.Level equals their base level
-         -- So we check for the last character in the spell's name, to check for upcasting
+         -- So we check for the last caster in the spell's name, to check for upcasting
         if lastCharSpell then
             spellLevel = lastCharSpell
         end
         _D(lastCharSpell)
-        if Osi.IsTagged(character,"dc8a847f-77b0-4dbb-a564-1ded034a1976") == 1 and spellLevel >= 1 and spellLevel <= 5 then
-            local characterHp = Osi.GetHitpoints(character)
-            local characterData = Ext.Stats.Get(character)
-            if characterHp < (spellLevel * 2) then
-                
+        if spellLevel >= 1 and spellLevel <= 5 then
+            local casterHp = Osi.GetHitpoints(caster)
+            local casterData = Ext.Stats.Get(caster)
+            if casterHp < (spellLevel * 2) then
+                Osi.Die(caster,10,caster,0,1,1)
             else
                 local boostName = "CONSUMPTIVE_POWER_MAX_HP_L" .. spellLevel
-                Osi.ApplyStatus(character,boostName,-1,100,character)
+                Osi.ApplyStatus(caster,boostName,-1,100,caster)
             end
         end
     end
